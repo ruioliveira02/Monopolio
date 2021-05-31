212145471662677c91a6ae22558eddbef7540d90
ruioliveira02 <rpoliveira02@gmail.com>
2021-05-31 21:43:56 +0100

    Added server

---
+++ NetworkModel/Requests/IdentRequest.cs
@@ -0,0 +1,15 @@
+﻿using Network.Packets;
+using System;
+
+namespace NetworkModel
+{
+    public class IdentRequest : RequestPacket
+    {
+        public IdentRequest(string username)
+        {
+            Username = username;
+        }
+
+        public string Username { get; set; }
+    }
+}
---
+++ NetworkModel/Responses/IdentResponse.cs
@@ -0,0 +1,20 @@
+﻿using Network.Attributes;
+using Network.Packets;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetworkModel
+{
+    [PacketRequest(typeof(IdentRequest))]
+    public class IdentResponse : ResponsePacket
+    {
+        public IdentResponse(bool accepted, RequestPacket request)
+            : base(request)
+        {
+            Accepted = accepted;
+        }
+
+        public bool Accepted { get; set; }
+    }
+}
