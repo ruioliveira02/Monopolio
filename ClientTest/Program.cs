f9be32316b8633345efdab42d51bb753e5557f4b
ruioliveira02 <rpoliveira02@gmail.com>
2021-06-04 15:49:31 +0100

    Revert "Added transmission of requests and responses"
    This reverts commit cac671cf6cbf1523f16d2884b3016eb04335684f.

--- ClientTest/Program.cs
+++ ClientTest/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Network;
 using NetworkModel;
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,7 +20,7 @@ namespace ClientTest
             Console.ReadLine();
         }
 
-        TcpClient clientSocket  = new TcpClient();
+        System.Net.Sockets.TcpClient clientSocket  = new System.Net.Sockets.TcpClient();
         NetworkStream serverStream  = default(NetworkStream);
         string readData = null;
 
@@ -36,21 +35,18 @@ namespace ClientTest
         public void button2_Click()
         {
             readData = "Conected to Chat Server ...";
-            //clientSocket.Connect("2.80.236.204", 25565);
-            clientSocket.Connect("127.0.0.1", 25565);
+            clientSocket.Connect("2.80.236.204", 25565);
             serverStream = clientSocket.GetStream();
 
-            IdentRequest request = new IdentRequest("membro");
-
-            byte[] outStream = Encoding.ASCII.GetBytes(request.ToJSON());
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes("ola$");
             serverStream.Write(outStream, 0, outStream.Length);
-            //serverStream.Flush();
+            serverStream.Flush();
 
-            Thread ctThread = new Thread(GetMessage);
+            Thread ctThread = new Thread(getMessage);
             ctThread.Start();
         }
 
-        private void GetMessage()
+        private void getMessage()
         {
             while (true)
             {
@@ -59,18 +55,9 @@ namespace ClientTest
                 byte[] inStream = new byte[100025];
                 buffSize = clientSocket.ReceiveBufferSize;
                 serverStream.Read(inStream, 0, buffSize);
-                string returndata = Encoding.ASCII.GetString(inStream);
-                if(returndata != null && returndata != "")
-                {
-                    JsonSerializerSettings settings = new JsonSerializerSettings
-                    {
-                        MetadataPropertyHandling = MetadataPropertyHandling.ReadAhead,
-                        TypeNameHandling = TypeNameHandling.All
-                    };
-                    returndata = returndata.Replace("MonopolioServer", "ClientTest").Replace("Monopolio Server", "ClientTest");
-                    Response response = JsonConvert.DeserializeObject(returndata, settings) as Response;
-                    response.Execute();
-                }
+                string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+                readData = "" + returndata;
+                Console.WriteLine(readData);
             }
         }
     }
