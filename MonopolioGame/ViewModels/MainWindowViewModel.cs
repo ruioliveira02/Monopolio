43e7d62c7c0c611f167ef6ded165c481788eb479
ruioliveira02 <rpoliveira02@gmail.com>
2021-06-09 18:59:22 +0100

    Added visual representation of player list

--- MonopolioGame/ViewModels/MainWindowViewModel.cs
+++ MonopolioGame/ViewModels/MainWindowViewModel.cs
@@ -9,6 +9,10 @@ namespace MonopolioGame.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        /// <summary>
+        /// The list of viewmodels for the list of players
+        /// </summary>
+        public ObservableCollection<PlayerViewModel> PlayersVM { get; set; }
         public ObservableCollection<PropertyViewModel> PropertiesVM { get; set; }
         public ObservableCollection<SpecialPlaceViewModel> CornersVM { get; set; }
         public string Username { get; set; }
@@ -25,8 +29,23 @@ namespace MonopolioGame.ViewModels
             ServerIp = "2.80.236.204:25565";
             LoginScreen = true;
             GameScreen = false;
+            SetBoard();
+            SetPlayers();
+        }
+
+        public void ConnectCommand()
+        {
+            string[] split = ServerIp.Split(":");
+            if (split.Length != 2 || !int.TryParse(split[1], out _)) //Invalid IP
+                return;//TODO:: Do stuff
+            else
+                handler.Connect(split[0], int.Parse(split[1]), Username);
+        }
+
+        private void SetBoard()
+        {
             var temp = new ObservableCollection<PropertyViewModel>();
-            for(int i = 0; i < 40; i++)
+            for (int i = 0; i < 40; i++)
             {
                 if (i % 10 == 0)
                     continue;
@@ -47,13 +66,15 @@ namespace MonopolioGame.ViewModels
             CornersVM = temp2;
         }
 
-        public void ConnectCommand()
+
+        private void SetPlayers()
         {
-            string[] split = ServerIp.Split(":");
-            if (split.Length != 2 || !int.TryParse(split[1], out _)) //Invalid IP
-                return;//TODO:: Do stuff
-            else
-                handler.Connect(split[0], int.Parse(split[1]), Username);
+            ObservableCollection<PlayerViewModel> temp = new ObservableCollection<PlayerViewModel>();
+            temp.Add(new PlayerViewModel(true, false, false, "Vasques", Brushes.Green, 10));
+            temp.Add(new PlayerViewModel(false, true, false, "Bace", Brushes.DarkBlue, -1000));
+            temp.Add(new PlayerViewModel(false, false, true, "Manela", Brushes.Pink, 100000)); //Roubou o dinheiro todo ao Vasques e foi se embora
+
+            PlayersVM = temp;
         }
     }
 }
